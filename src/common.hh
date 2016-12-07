17eb91f5a22de44f3395c5544fd8b090e4e5fd3d
Simone Bernardi <simone@mygoodclient.com>
2016-12-07 20:38:57 +0100

    Added slugify() function

--- src/common.hh
+++ src/common.hh
@@ -119,3 +119,22 @@ function regex_all(string $pattern, string $subject, ?int &$matches = null) {
   $matches = $m;
   return $out;
 }
+
+function slugify($text) {
+  // replace non letter or digits by -
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+  // trim
+  $text = trim($text, '-');
+  // remove duplicate -
+  $text = preg_replace('~-+~', '-', $text);
+  // lowercase
+  $text = strtolower($text);
+  if (empty($text)) {
+    return 'n-a';
+  }
+  return $text;
+}
