6d9a1523304bc5cd17a79abeabbb52222092fdf1
Simone Bernardi <simone@mygoodclient.com>
2018-04-11 11:10:41 +0200

    Added recursive_array_diff() function in common.hh

--- src/common.hh
+++ src/common.hh
@@ -138,3 +138,24 @@ function slugify($text) {
   }
   return $text;
 }
+
+function recursive_array_diff($a1, $a2) {
+  $r = array();
+  foreach ($a1 as $k => $v) {
+    if (array_key_exists($k, $a2)) {
+      if (is_array($v)) {
+        $rad = recursive_array_diff($v, $a2[$k]);
+        if (count($rad)) {
+          $r[$k] = $rad;
+        }
+      } else {
+        if ($v != $a2[$k]) {
+          $r[$k] = $v;
+        }
+      }
+    } else {
+      $r[$k] = $v;
+    }
+  }
+  return $r;
+}
